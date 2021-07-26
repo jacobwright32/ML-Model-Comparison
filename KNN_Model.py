25d881de73b88e8aae80727df1f22b793085fa60
jacobwright32 <jacoblouiswright@msn.com>
2021-07-26 21:11:11 +0100

    Updates the KNN_Model

--- KNN_Model.py
+++ KNN_Model.py
@@ -1,14 +1,18 @@
 #%%
 from get_data import split_data
+from sklearn import neighbors
 from sklearn.neighbors import KNeighborsRegressor
 
 X_train, X_val, X_test, y_train, y_val, y_test = split_data()
+k = 5
 
 def KNN_Model(X, X_val, y, y_val):
-    knn = KNeighborsRegressor(n_neighbors=5)
-    y_ = knn.fit(X, y)
-
+    
+    knn = neighbors.KNeighborsRegressor(n_neighbors=k)
+    knn.fit(X, y)
+    return print(knn.score(X_val, y_val))
 
 KNN_Model(X_train, X_val, y_train, y_val)
 
+# %%
 # %%
