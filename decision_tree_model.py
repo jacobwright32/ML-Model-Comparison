8616b5a9c4e1d4e4d61543680e8510503c7eaf04
Spiros07 <s.morfonios@gmail.com>
2021-07-26 21:21:41 +0100

    added train, val and test

--- decision_tree_model.py
+++ decision_tree_model.py
@@ -2,6 +2,8 @@
 from sklearn import tree
 from get_data import split_data
 
+
+# Getting a boston dataset splited by train, validation, and test set
 X_train, X_val, X_test, y_train, y_val, y_test = split_data()
 
 
@@ -9,14 +11,27 @@ def decision_tree(X, y):
 
     clf = tree.DecisionTreeRegressor()
 
+    # Train the model
     clf = clf.fit(X, y)
     
     clf.score(X, y)
 
-    #tree.plot_tree(clf)
+    # Validate the model
+    result = {
+        'train_score': clf.score(X_train, y_train),
+        'val_score': clf.score(X_val, y_val),
+        'test_score': clf.score(X_test, y_test),
+    }
 
-    
-decision_tree(X_train, y_train)
+    return result
+
+train_result = decision_tree(X_train,y_train)
+val_result = decision_tree(X_val,y_val)
+test_result = decision_tree(X_test,y_test)
+
+print(train_result)
+print(val_result)
+print(test_result)
 
 
 # %%
