8215b9f7a6da99f415eca4987a181e863043ea2a
thisisyoojin <thisisyoojin@gmail.com>
2021-07-26 20:33:29 +0100

    Fixes a typo and train the model

--- SVM_model.py
+++ SVM_model.py
@@ -1,5 +1,14 @@
-from sklearn.svm import SVM
+from sklearn.svm import SVC
 from get_data import split_data
 
+# Getting a boston dataset splited by train, validation, and test set
+X_train, X_val, X_test, y_train, y_val, y_test = split_data()
 
+# Call the SVC
+clf = SVC()
 
+# Train the model
+clf.fit(X_train, y_train)
+
+# Validate the model
+clf.score(X_val, y_val)
