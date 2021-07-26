34529e518d9126734ed6186482f79d2dc245fbb8
thisisyoojin <thisisyoojin@gmail.com>
2021-07-26 20:57:56 +0100

    Adds a function to generate params

--- SVM_model.py
+++ SVM_model.py
@@ -2,27 +2,40 @@ from sklearn.svm import SVR
 from get_data import split_data
 
 
-def train_SVR(**params):
+def train_SVR():
 
     # Getting a boston dataset splited by train, validation, and test set
     X_train, X_val, X_test, y_train, y_val, y_test = split_data()
 
     # Call the SVC
-    clf = SVR(**params)
+    params = generate_params()
+    reg = SVR(**params)
 
     # Train the model
-    clf.fit(X_train, y_train)
+    reg.fit(X_train, y_train)
 
     # Validate the model
-    print(clf.score(X_val, y_val))
+    result = {
+        'train_score': reg.score(X_train, y_train),
+        'val_score': reg.score(X_val, y_val),
+        'test_score': reg.score(X_test, y_test),
+    }
+
+    return result
+    
+
+def generate_params():
+    
+    params = {
+        #'kernel' : ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed'],
+        'kernel': 'rbf',
+        'gamma': 0.01,
+        'C': 10,
+    }
+
+    return params
 
 
-params = {
-    'kernel' : 'rbf',
-    'degree': 3,
-    'gamma': 'auto',
-    'C': 10
-}
-
-train_SVR(**params)
+result = train_SVR()
+print(result)
 
