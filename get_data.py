f65062db06c2a955bc8874c1b60d523387a8e593
jacobwright32 <jacoblouiswright@msn.com>
2021-07-26 20:28:30 +0100

    loads in the data

--- get_data.py
+++ get_data.py
@@ -3,11 +3,10 @@ import numpy as np
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 
-random_seed = np.random.randint(1024)
 
 def split_data():
-    
+    random_seed_gen = np.random.randint(1024)
     X, y = load_boston(return_X_y=True)
-    X_train, X_test, y_train, y_test = train_test_split(X, y)   
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_seed=random_seed_gen)   
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, random_seed=random_seed_gen)
     return X_train, X_val, X_test, y_train, y_val, y_test
