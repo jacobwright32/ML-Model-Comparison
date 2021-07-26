955c96d4d724f19a6461e23b53413243d703abce
jacobwright32 <jacoblouiswright@msn.com>
2021-07-26 20:47:04 +0100

    Updates KNN_Model.py

--- KNN_Model.py
+++ KNN_Model.py
@@ -1,15 +1,15 @@
 #%%
 from get_data import split_data
-from sklearn.neighbors import NearestNeighbors
-
+from sklearn.neighbors import KNeighborsClassifier
 X_train, X_val, X_test, y_train, y_val, y_test = split_data()
 
-def KNN_Model(X, y):
-    neigh = NearestNeighbors(n_neighbors=2)
-    neigh.fit(X,y)
-    
+def KNN_Model(X,X_val, y, y_val):
+    knn = KNeighborsClassifier(n_neighbors=2)
+    knn.fit(X,y)
+    y_pred = knn.predict(X_val)
+    knn.score(y_val,y_pred)
 
-    
-KNN_Model(X_train, y_train)
+
+KNN_Model(X_train, X_val, y_train, y_val)
 
 # %%
