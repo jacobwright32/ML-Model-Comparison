7d86a3bbb0adaabd87e42c54165cb3f81d9d33b8
jacobwright32 <jacoblouiswright@msn.com>
2021-07-27 19:17:24 +0100

    updates KNN_Model

--- KNN_Model.py
+++ KNN_Model.py
@@ -5,15 +5,17 @@ from sklearn.neighbors import KNeighborsRegressor
 
 X_train, X_val, X_test, y_train, y_val, y_test = split_data()
 k = 4
-
 def KNN_Model(X, X_val, y, y_val):
     
     knn = neighbors.KNeighborsRegressor(n_neighbors=k)
     knn.fit(X, y)
     return print(knn.score(X_val, y_val))
 
+def hyper_para():
+    for k in range(1,30):
+            KNN_Model(X_train, X_val, y_train, y_val, k)
 
 KNN_Model(X_train, X_val, y_train, y_val)
-
+# %%
 
 # %%
