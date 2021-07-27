34c090e98d352c2df47e5523923a74a12d0e3c39
thisisyoojin <thisisyoojin@gmail.com>
2021-07-27 20:17:18 +0100

    Adds hyperparameter

--- SVM_model.py
+++ SVM_model.py
@@ -1,3 +1,4 @@
+#%%
 import numpy as np
 import pandas as pd
 import itertools
@@ -5,7 +6,7 @@ from sklearn.svm import SVR
 from get_data import split_data
 
 
-def train_SVR():
+def SVR_model():
 
     # Getting a boston dataset splited by train, validation, and test set
     X_train, X_val, X_test, y_train, y_val, y_test = split_data()
@@ -17,19 +18,19 @@ def train_SVR():
     for r in grid_params:
         
         param = {keys[idx]:r[idx] for idx in range(len(r))}
-        print(param)
+        
         reg = SVR(**param)
         reg.fit(X_train, y_train)
         
         # Train the model with grid search
-        result = {
+        scores = {
             'params': param,
             'train_score': reg.score(X_train, y_train),
             'val_score': reg.score(X_val, y_val),
             'test_score': reg.score(X_test, y_test),
         }
-
-        results.append(result)
+        #param.update(scores)
+        results.append(scores)
 
     return results
     
@@ -49,7 +50,6 @@ def generate_params():
     return keys, values, grid_params
     
 
-results = train_SVR()
+results = SVR_model()
 df = pd.DataFrame(results)
-
-# %%
+df
