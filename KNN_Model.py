9ea4b62de667fdda67d216c518e6292690b5bd12
Jake Wright <84330936+jacobwright32@users.noreply.github.com>
2021-07-28 19:33:06 +0100

    Knn model (#12)
    * branching
    
    * adds a KNN branch
    
    * adds all the score to model comparison file
    
    * Updates KNN_model
    
    * Adds algorithm hyperparams to the KNN_Model
    
    * deletes typo in model_comparison file
    
    * renames model_comparison file
    
    * trying to resolve merging issue
    
    * trying to resolve merging issue
    
    * adds to the main
    
    * adds score list to a dataframe
    
    * reformats KNN_Model to apropriate stucture
    
    * updates the KNN_Model to work with base.py
    
    Co-authored-by: jacobwright32 <jacoblouiswright@msn.com>

--- KNN_Model.py
+++ KNN_Model.py
@@ -1,53 +1,14 @@
 #%%
-from numpy.lib.npyio import savez_compressed
-from get_data import split_data
-import itertools
-from sklearn import neighbors
+from sklearn.svm import SVR
 from sklearn.neighbors import KNeighborsRegressor
-import pandas as pd
+from Base import Base
 
-X_train, X_val, X_test, y_train, y_val, y_test = split_data()
+class KNN_model(Base):
 
-
-def KNN_Model():
-        
-    keys, values, grid_params = generate_params()
-    results = []
-    
-    for r in grid_params:
-        param = {keys[idx]:r[idx] for idx in range(len(r))}
-        print(param)
-        knn = neighbors.KNeighborsRegressor(**param)
-        knn.fit(X_train, y_train)    
-        
-        result = {
-                    'param' : param,
-                'train_score': knn.score(X_train, y_train),
-                'val_score': knn.score(X_val, y_val),
-                'test_score': knn.score(X_test, y_test)
-            }
-    results.append(result)
-    
-    return results
-
-def generate_params():
-    scores = []
-    
-    params = {
+    def __init__(self):
+        super().__init__(KNeighborsRegressor)
+        self.params = {
         'algorithm' : ['auto', 'ball_tree', 'kd_tree', 'brute'],
         'n_neighbors': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
         'leaf_size': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
     }
-    
-    keys = list(params.keys())
-    values = params.values()
-    grid_params = list(itertools.product(*values))    
-    return keys, values, grid_params
-
-results = KNN_Model()
-
-df = pd.DataFrame(results)
-
-print(df)
-# %%
-
