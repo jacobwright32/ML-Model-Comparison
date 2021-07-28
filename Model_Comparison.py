101ded4345322017621e079c6819296220087e63
thisisyoojin <thisisyoojin@gmail.com>
2021-07-28 20:01:34 +0100

    Finds a best models

--- Model_Comparison.py
+++ Model_Comparison.py
@@ -1,6 +1,11 @@
 #%%
-from KNN_Model import hyper_para
+from KNN_Model import KNN_model
 from SVM_model import SVR_model
+from decision_tree_model import Decision_tree
+from get_data import split_data
 
-knn_scores = hyper_para()
-print(knn_scores[len(knn_scores)-1][1])
+
+best_knn = KNN_model().best_model()
+best_decision_tree = Decision_tree().best_model()
+best_svr = SVR_model().best_model()
+
