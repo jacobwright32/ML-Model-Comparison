fbd18af1b3ea5538c82d16e2c6097aacdda15660
jacobwright32 <jacoblouiswright@msn.com>
2021-07-28 13:58:42 +0100

    trying to resolve merging issue

--- Model_Comparison.py.py
+++ Model_Comparison.py.py
@@ -1,11 +1,5 @@
 #%%
-from numpy.lib.npyio import _savez_compressed_dispatcher
 from KNN_Model import hyper_para
-from SVM_model import SVR_model
-from decision_tree_model import decision_tree
 
 knn_scores = hyper_para()
-SVR_scores = SVR_model()
-Decision_Tree_scores = decision_tree()
-print(SVR_scores)
-print(knn_scores)
+print(knn_scores[len(knn_scores)-1][1])
