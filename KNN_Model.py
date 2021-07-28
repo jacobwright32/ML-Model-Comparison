ab36fd472a341569269ecc55699fd5d9d4d6702d
Jake Wright <84330936+jacobwright32@users.noreply.github.com>
2021-07-28 19:25:36 +0100

    Knn model (#11)
    * branching
    
    * adds a KNN branch
    
    * adds all the score to model comparison file
    
    * Updates KNN_model
    
    * Adds algorithm hyperparams to the KNN_Model
    
    * deletes typo in model_comparison file
    
    * renames model_comparison file
    
    * trying to resolve merging issue
    
    * trying to resolve merging issue
    
    * adds to the main
    
    * adds score list to a dataframe
    
    * reformats KNN_Model to apropriate stucture
    
    Co-authored-by: jacobwright32 <jacoblouiswright@msn.com>

--- KNN_Model.py
+++ KNN_Model.py
@@ -1,40 +1,53 @@
 #%%
 from numpy.lib.npyio import savez_compressed
 from get_data import split_data
+import itertools
 from sklearn import neighbors
 from sklearn.neighbors import KNeighborsRegressor
+import pandas as pd
 
 X_train, X_val, X_test, y_train, y_val, y_test = split_data()
 
-k = 4
-leaf = 30
-algo = 'auto'
 
-def KNN_Model(X_train, X_val, X_test, y_train, y_val, y_test, algo, k, leaf):
-    knn = neighbors.KNeighborsRegressor(n_neighbors=k, leaf_size=leaf, algorithm=algo)
-    knn.fit(X_train, y_train)    
-    score = {
-                   'param' : [algo, k, leaf],
-            'train_score': knn.score(X_train, y_train),
-            'val_score': knn.score(X_val, y_val),
-            'test_score': knn.score(X_test, y_test)
-        }
-    
-    return score
-
-def hyper_para():
-    scores = []
-    for algo in ['auto', 'ball_tree', 'kd_tree', 'brute']:
-
-        for k in range(10,100,10):
-            
-            for leaf in range(1,30):
-                scores.append(KNN_Model(X_train, X_val, X_test, y_train, y_val, y_test, algo, k, leaf))
+def KNN_Model():
         
-    return scores
+    keys, values, grid_params = generate_params()
+    results = []
+    
+    for r in grid_params:
+        param = {keys[idx]:r[idx] for idx in range(len(r))}
+        print(param)
+        knn = neighbors.KNeighborsRegressor(**param)
+        knn.fit(X_train, y_train)    
+        
+        result = {
+                    'param' : param,
+                'train_score': knn.score(X_train, y_train),
+                'val_score': knn.score(X_val, y_val),
+                'test_score': knn.score(X_test, y_test)
+            }
+    results.append(result)
+    
+    return results
 
-knn_scores = hyper_para()
-print(knn_scores)
+def generate_params():
+    scores = []
+    
+    params = {
+        'algorithm' : ['auto', 'ball_tree', 'kd_tree', 'brute'],
+        'n_neighbors': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        'leaf_size': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    }
+    
+    keys = list(params.keys())
+    values = params.values()
+    grid_params = list(itertools.product(*values))    
+    return keys, values, grid_params
+
+results = KNN_Model()
+
+df = pd.DataFrame(results)
+
+print(df)
 # %%
 
-# %%
--- Model_Comparison.py.py
+++ Model_Comparison.py.py
@@ -4,9 +4,3 @@ from SVM_model import SVR_model
 
 knn_scores = hyper_para()
 print(knn_scores[len(knn_scores)-1][1])
-
-
-
-svr = SVR_model()
-results = svr.fit()
-print(results)
