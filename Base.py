5d13d519f1cfd5045ad62b17cbdfd2454b972882
thisisyoojin <thisisyoojin@gmail.com>
2021-07-28 18:57:42 +0100

    Updates base file

--- Base.py
+++ Base.py
@@ -1,17 +1,44 @@
+import itertools
 from get_data import split_data
 
 
 class Base:
-    def __init__(self):
-        X_train, X_val, X_test, y_train, y_val, y_test = split_data()
+    def __init__(self, model):
+        self.X_train, self.X_val, self.X_test, self.y_train, self.y_val, self.y_test = split_data()
+        self.model = model
 
 
-    def generate_params(self):
-        pass
+    def generate_params(self, params):
+        
+        keys = list(params.keys())
+        values = params.values()
+        grid_params = list(itertools.product(*values))
+
+        return keys, values, grid_params
 
 
     def fit(self):
-        pass
+        keys, values, grid_params = self.generate_params()
+        results = []
+        
+        for r in grid_params:
+                
+                param = {keys[idx]:r[idx] for idx in range(len(r))}
+                print(param)
+                reg = self.model(**param)
+                reg.fit(self.X_train, self.y_train)
+                
+                # Train the model with grid search
+                result = {
+                    'params': param,
+                    'train_score': reg.score(self.X_train, self.y_train),
+                    'val_score': reg.score(self.X_val, self.y_val),
+                    'test_score': reg.score(self.X_test, self.y_test),
+                }
+
+                results.append(result)
+
+        return results
 
 
         
