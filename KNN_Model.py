743765d0abf16f915692b6f2fb0f03c1acab465c
Jake Wright <84330936+jacobwright32@users.noreply.github.com>
2021-07-28 14:08:46 +0100

    Knn model (#8)
    * branching
    
    * adds a KNN branch
    
    * adds all the score to model comparison file
    
    * Updates KNN_model
    
    * Adds algorithm hyperparams to the KNN_Model
    
    * deletes typo in model_comparison file
    
    * renames model_comparison file
    
    * trying to resolve merging issue
    
    * trying to resolve merging issue
    
    Co-authored-by: jacobwright32 <jacoblouiswright@msn.com>

--- KNN_Model.py
+++ KNN_Model.py
@@ -1,30 +1,40 @@
 #%%
+from numpy.lib.npyio import savez_compressed
 from get_data import split_data
 from sklearn import neighbors
 from sklearn.neighbors import KNeighborsRegressor
 
 X_train, X_val, X_test, y_train, y_val, y_test = split_data()
-<<<<<<< HEAD
-k = 2
 
-def KNN_Model(X, X_val, y, y_val):
-=======
 k = 4
-def KNN_Model(X, X_val, y, y_val, k):
->>>>>>> 5890a690aa4cb6d17e1de860a9c61d5a36f5e7a3
+leaf = 30
+algo = 'auto'
+
+def KNN_Model(X_train, X_val, X_test, y_train, y_val, y_test, algo, k, leaf):
+    knn = neighbors.KNeighborsRegressor(n_neighbors=k, leaf_size=leaf, algorithm=algo)
+    knn.fit(X_train, y_train)    
+    score = {
+                   'param' : [algo, k, leaf],
+            'train_score': knn.score(X_train, y_train),
+            'val_score': knn.score(X_val, y_val),
+            'test_score': knn.score(X_test, y_test)
+        }
     
-    knn = neighbors.KNeighborsRegressor(n_neighbors=k)
-    knn.fit(X, y)
-    return knn.score(X_val, y_val)
+    return score
 
 def hyper_para():
-    scores = {}
-    for k in range(1,30):
-            scores['k:' + str(k)] = KNN_Model(X_train, X_val, y_train, y_val, k)
-    return sorted(scores.items(), key=lambda item: item[1])
+    scores = []
+    for algo in ['auto', 'ball_tree', 'kd_tree', 'brute']:
+
+        for k in range(10,100,10):
+            
+            for leaf in range(1,30):
+                scores.append(KNN_Model(X_train, X_val, X_test, y_train, y_val, y_test, algo, k, leaf))
+        
+    return scores
 
 knn_scores = hyper_para()
 print(knn_scores)
 # %%
 
-
+# %%
